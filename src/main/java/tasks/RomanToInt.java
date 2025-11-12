a0262ff527b8304748aecfa68fcedf73a40ccad6
ychekulaeva <k307002@deluxe.com>
2025-11-12 15:51:13 +0200

    RomanToInt addition

--- src/main/java/tasks/RomanToInt.java
+++ src/main/java/tasks/RomanToInt.java
@@ -6,9 +6,12 @@ public class RomanToInt {
 
     public static void main(String[] args) {
 
+        System.out.println(romanToInt("III"));
+        System.out.println(romanToInt("LVIII"));
+        System.out.println(romanToInt("MCMXCIV"));
     }
 
-    public int romanToInt(String s) {
+    public static int romanToInt(String s) {
         int result = 0;
         HashMap<String, Integer> keys = new HashMap<String, Integer>();
         keys.put("I", 1);
@@ -19,10 +22,20 @@ public class RomanToInt {
         keys.put("D", 500);
         keys.put("M", 1000);
 
-        for (int i = s.length()-1; i >= 0; i--) {
-
+        for (int i = s.length(); i > 0; i--) {
+            String s0 = s.substring(i - 1, i);
+            int current = keys.get(s0);
+            if (i == s.length()) {
+                result += current;
+            } else {
+                int prev = keys.get(s.substring(i, i + 1));
+                if (prev > current) {
+                    result -= current;
+                } else {
+                    result += current;
+                }
+            }
         }
         return result;
-
     }
 }
